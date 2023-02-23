83a054ffac7a9ce43606b44965b98db3af3ad4fa
MrBri <659209+MrBri@users.noreply.github.com>
2023-02-23 15:50:45 -0800

    feat: more tests for lru

--- Prob_1_LRU_Cache/problem_1.py
+++ Prob_1_LRU_Cache/problem_1.py
@@ -17,7 +17,7 @@ class LRU_Cache(object):
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
         if len(self.cache) >= self.cap:
-            self.cache.popitem(last=False) # remove oldest
+            self.cache.popitem(last=False) # Remove oldest
         self.cache[key] = value
 
 our_cache = LRU_Cache(5)
@@ -42,8 +42,47 @@ print('get 3', our_cache.get(3), our_cache.cache, our_cache.cap)      # returns 
 
 # Test Case 1
 
+cache = LRU_Cache(2)
+cache.set(0, 0)
+cache.set(0, 0)
+cache.set(0, 0)
+
+print('get 0', cache.get(0), cache.cache, cache.cap)       # returns 0
+print('get 0', cache.get(0), cache.cache, cache.cap)       # returns 0
+print('get 0', cache.get(0), cache.cache, cache.cap)       # returns 0
+
 # Test Case 2
+cache = LRU_Cache(2)
+cache.set("", "")
+cache.set("", "")
+cache.set("", "")
+
+print('get blank', cache.get(""), cache.cache, cache.cap)       # returns ''
+print('get 0', cache.get(0), cache.cache, cache.cap)       # returns -1
 
 # Test Case 3
+cache = LRU_Cache(10)
+cache.set(1, 1)
+cache.set(2, 2)
+cache.set(3, 3)
+cache.set(1, 1)
+cache.set(2, 2)
+cache.set(3, 3)
+cache.set(1, 1)
+cache.set(2, 2)
+cache.set(3, 3)
+cache.set(4, 4)
+cache.set(5, 5)
+cache.set(6, 6)
+cache.set(7, 7)
+cache.set(8, 8)
+cache.set(9, 9)
+cache.set(10, 10)
+cache.set(11, 11)
+cache.set(12, 12)
+
+print('get 1', cache.get(1), cache.cache, cache.cap)       # returns 0
+print('get 0', cache.get(0), cache.cache, cache.cap)       # returns 0
+print('get 3', cache.get(3), cache.cache, cache.cap)       # returns 0
 
 # Show Me the Data Structures
--- Prob_6_Union_Intersection/problem_6.py
+++ Prob_6_Union_Intersection/problem_6.py
@@ -65,8 +65,22 @@ def union(llist_1, llist_2):
     return union_list
 
 def intersection(llist_1, llist_2):
-    # Your Solution Here
-    pass
+    common = dict()
+    output = LinkedList()
+
+    curr = llist_1.head
+    while curr is not None:
+        common[curr.value] = True
+        curr = curr.next
+
+    curr = llist_2.head
+    while curr is not None:
+        if curr.value in common:
+            output.append(curr.value)
+            del common[curr.value] # remove deplicates from output
+        curr = curr.next
+
+    return output
 
 
 # Test case 1
@@ -84,7 +98,7 @@ for i in element_2:
     linked_list_2.append(i)
 
 print (union(linked_list_1,linked_list_2)) # 32 -> 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 9 -> 11 -> 21 -> 
-# print (intersection(linked_list_1,linked_list_2))
+print (intersection(linked_list_1,linked_list_2))
 
 # Test case 2
 
@@ -101,7 +115,7 @@ for i in element_2:
     linked_list_4.append(i)
 
 print (union(linked_list_3,linked_list_4)) # 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 7 -> 8 -> 9 -> 11 -> 21 -> 23 ->
-# print (intersection(linked_list_3,linked_list_4))
+print (intersection(linked_list_3,linked_list_4))
 
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
@@ -121,90 +135,38 @@ for i in element_2:
     linked_list_2.append(i)
 
 print (union(linked_list_1,linked_list_2)) #
-# print (intersection(linked_list_1,linked_list_2))
+print (intersection(linked_list_1,linked_list_2))
 # Test Case 2
 
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
+
+element_1 = [6, 6, 6]
+element_2 = [6, 6, 6, 7]
+
+for i in element_1:
+    linked_list_1.append(i)
+
+for i in element_2:
+    linked_list_2.append(i)
+
+print (union(linked_list_1,linked_list_2)) #
+print (intersection(linked_list_1,linked_list_2))
 # Test Case 3
 
 
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
 
+element_1 = []
+element_2 = []
 
+for i in element_1:
+    linked_list_1.append(i)
 
-'''
-def union(llist_1, llist_2):
-    # create a set to store the values in the union
-    union_set = set()
+for i in element_2:
+    linked_list_2.append(i)
 
-    # traverse the first linked list and add the values to the set
-    current_node = llist_1
-    while current_node:
-        union_set.add(current_node.value)
-        current_node = current_node.next
+print (union(linked_list_1,linked_list_2)) #
+print (intersection(linked_list_1,linked_list_2))
 
-    # traverse the second linked list and add the values to the set
-    current_node = llist_2
-    while current_node:
-        union_set.add(current_node.value)
-        current_node = current_node.next
-
-    # create a new linked list with the values in the union set
-    union_list = Node()
-    current_node = union_list
-    for value in union_set:
-        current_node.next = Node(value)
-        current_node = current_node.next
-
-    return union_list.next
-
-def intersection(llist_1, llist_2):
-    # create a set to store the values in the intersection
-    intersection_set = set()
-
-    # traverse the first linked list and add the values to a set
-    current_node = llist_1
-    while current_node:
-        intersection_set.add(current_node.value)
-        current_node = current_node.next
-
-    # traverse the second linked list and add the values to a set
-    current_node = llist_2
-    while current_node:
-        if current_node.value in intersection_set:
-            current_node = current_node.next
-        elif :
-            intersection_set.remove(current_node.value)
-
-    # create a new linked list with the values in the intersection set
-    intersection_list = Node()
-    current_node = intersection_list
-    for value in intersection_set:
-        current_node.next = Node(value)
-        current_node = current_node.next
-
-    return intersection_list.next
-
-# create the first linked list
-llist_1 = Node(1)
-llist_1.next = Node(2)
-llist_1.next.next = Node(3)
-
-# create the second linked list
-llist_2 = Node(2)
-llist_2.next = Node(3)
-llist_2.next.next = Node(4)
-
-# test the union function
-union_list = union(llist_1, llist_2)
-current_node = union_list
-while current_node:
-    print(current_node.value)
-    current_node = current_node.next
-
-print('intersection')
-# test the intersection function
-intersection_list = intersection(llist_1, llist_2)
-current_node = intersection_list
-while current_node:
-    print(current_node.value)
-    current_node = current_node.next
-'''
